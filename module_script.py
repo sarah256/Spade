4aaeb740bd8054e4e2f302b7211580ba56d0e42e
sarah256 <srieger@bu.edu>
2018-07-12 22:57:56 -0400

    Add error to be thrown when a dependency is spotted

--- module_script.py
+++ module_script.py
@@ -11,15 +11,23 @@ blacklist_br = set(blacklist.get('buildrequires', []))
 while True:
     response = requests.get(url).json()
     for module in response['items']:
-        modulemd = module['modulemd']
+        modulemd = yaml.safe_load(module['modulemd'])
         dependencies = modulemd['dependencies']
         module_br = set()
         module_req = set()
         for dependency in dependencies:
             module_br = module_br.union(set(dependency.get('buildrequires', {}).keys()))
             module_req = module_req.union(set(dependency.get('requires', {}).keys()))
-        # compare sets, print out offending modules and add error message of why its an offender
-        # specify buildrequires or requires
+        overlap_br = set(blacklist_br).intersection(set(module_br))
+        overlap_req = set(blacklist_req).intersection(set(module_req))
+        if overlap_br:
+            modules.append(module)
+            raise RuntimeError('The module {0} with the ID {1} has a buildrequires dependency on {2}').format(
+                module['name'], module['id'], ', '.join(str(x) for x in overlap_br))
+        if overlap_req:
+            modules.append(module)
+            raise RuntimeError('The module {0} with the ID {1} has a requires dependency on {2}').format(
+                module['name'], module['id'], ', '.join(str(x) for x in overlap_br))
     if response['meta']['next']:
         url = response['meta']['next']
     else:
