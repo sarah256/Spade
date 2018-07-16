203a93d9bd085aac70d862e4f946b3706815d75a
sarah256 <srieger@bu.edu>
2018-07-16 09:22:56 -0400

    Accomodate for files that are not upgraded

--- module_script.py
+++ module_script.py
@@ -1,10 +1,18 @@
+from __future__ import print_function
+import sys
 import requests
 import yaml
+import json
+
+import gi
+gi.require_version('Modulemd', '1.0')  # noqa
+from gi.repository import Modulemd  # noqa
+
 
 url = 'https://mbs.engineering.redhat.com/module-build-service/1/module-builds/?state=ready&per_page=100&verbose=True'
-modules = []
 with open('blacklist.yaml', 'r') as blacklist_file:
     blacklist = yaml.safe_load(blacklist_file)
+modules = []
 blacklist_req = set(blacklist.get('requires', []))
 blacklist_br = set(blacklist.get('buildrequires', []))
 
@@ -12,23 +20,33 @@ while True:
     response = requests.get(url).json()
     for module in response['items']:
         modulemd = yaml.safe_load(module['modulemd'])
-        dependencies = modulemd['data']['dependencies']
+        modulemd = json.dumps(modulemd)
+        try:
+            mmd = Modulemd.Module().new_from_string(modulemd)
+            mmd.upgrade()
+        except Exception:
+            raise ValueError('Invalid modulemd')
+        mmd = json.loads(mmd)
+        dependencies = mmd['data'].props.dependencies
         module_br = set()
         module_req = set()
         for dependency in dependencies:
-            module_br = module_br.union(set(dependency.get('buildrequires', {}).keys()))
-            module_req = module_req.union(set(dependency.get('requires', {}).keys()))
+            module_br = module_br.union(set(dependency.get_buildrequires().keys()))
+            module_req = module_req.union(set(dependency.get_requires().keys()))
         overlap_br = set(blacklist_br).intersection(set(module_br))
         overlap_req = set(blacklist_req).intersection(set(module_req))
         if overlap_br:
             modules.append(module)
-            print('The module {0} with the ID {1} has a buildrequires dependency on {2}'.format(
-                module['name'], module['id'], ', '.join(str(x) for x in overlap_br)))
+            print('\nThe module {0} with the ID {1} has a buildrequires dependency on {2}\n'.format(
+                module['name'], module['id'], ', '.join(str(x) for x in overlap_br)), file=sys.stderr)
         if overlap_req:
             modules.append(module)
-            print('The module {0} with the ID {1} has a requires dependency on {2}'.format(
-                module['name'], module['id'], ', '.join(str(x) for x in overlap_req)))
+            print('\nThe module {0} with the ID {1} has a requires dependency on {2}\n'.format(
+                module['name'], module['id'], ', '.join(str(x) for x in overlap_req)), file=sys.stderr)
     if response['meta']['next']:
         url = response['meta']['next']
     else:
         break
+
+if modules:
+    sys.exit(1)
